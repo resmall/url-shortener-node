193da03c68bf5bb43647597de1fab05db848757f
Tiago R. Lammers <trlammers@gmail.com>
2019-07-06 10:29:32 -0300

    WIP: tests passing

--- src/index.js
+++ src/index.js
@@ -4,16 +4,20 @@ const router = new Router();
 const app = new Koa();
 const PORT = process.env.PORT || 8081;
 
-router.get("/", async ctx => {
-  ctx.body = {
-    data: "Sending some JSON"
-  };
+router.get('/', async ctx => {
+    ctx.body = {
+        data: 'URL Shorten API'
+    };
+});
+
+router.post('/shorten', async ctx => {
+    ctx.body = {newUrl: 'http://localhost/'}
 });
 
 app.use(router.routes());
 
-const server = app.listen(PORT).on("error", err => {
-  console.error(err);
+const server = app.listen(PORT).on('error', err => {
+    console.error(err);
 });
 
 module.exports = server;
--- src/tests/integration/shorten.test.js
+++ src/tests/integration/shorten.test.js
@@ -10,3 +10,13 @@ test("Response", async () => {
     const res = await request(app).get('/');
     expect(res.status).toEqual(200);
 })
+
+test("Requests a new url", async () => {
+    const res = await request(app)
+        .post('/shorten')
+        .send({
+            url: "http://google.com"
+        });
+    expect(res.status).toEqual(200);
+    expect(res.body).toHaveProperty('newUrl')
+})
