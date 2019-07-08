43c0c4f89f17f17d306f4417a551d9f8ed2bf4f5
Tiago R. Lammers <trlammers@gmail.com>
2019-07-08 14:50:08 -0300

    refactored controllers

---
+++ src/controllers/UrlController.js
@@ -0,0 +1,59 @@
+const UrlModel = require('../models/url');
+const Url = require('../url/url');
+const { getExpirationDate, getUnixTimestamp } = require('../services/date');
+
+module.exports = {
+  health(ctx) {
+    ctx.body = {
+      data: 'URL Shorten API',
+    };
+  },
+
+  async shorten(ctx) {
+    const { url } = ctx.request.body;
+
+    if (url && Url.isValid(url)) {
+      const expirationDate = getExpirationDate(process.env.SECONDS_TO_EXPIRE_URL);
+      const expirationTimeUnixTimestamp = getUnixTimestamp(expirationDate);
+
+      try {
+        const saved = await UrlModel.create({
+          url,
+          expires_at: expirationDate,
+        });
+
+        ctx.body = {
+          newUrl: `${process.env.SERVICE_URL}/${saved._id}`,
+          expiresAt: expirationTimeUnixTimestamp,
+        };
+      } catch (e) {
+        console.error(e);
+        ctx.throw(500, 'There was a problem with your request.');
+      }
+    } else {
+      ctx.status = 400;
+      ctx.body = {
+        message: 'Invalid request.',
+      };
+    }
+  },
+
+  async get(ctx) {
+    const { id } = ctx.params;
+
+    if (id) {
+      try {
+        const url = await UrlModel.findById(id);
+        if (url) {
+          ctx.redirect(url.url);
+          return;
+        }
+      } catch (e) {
+        console.error(e);
+        ctx.throw(500, 'There was a problem with your request.');
+      }
+    }
+    ctx.body = { message: 'Url not found.' };
+    ctx.status = 404;
+  },
+};
--- src/index.js
+++ src/index.js
@@ -5,15 +5,13 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
 const mongoose = require('mongoose');
-const UrlModel = require('./models/url');
-
-const Url = require('./url/url');
-const { getExpirationDate, getUnixTimestamp } = require('./services/date');
 
 const router = new Router();
 const app = new Koa();
 
 const PORT = process.env.PORT || 3000;
+
+const urlController = require('./controllers/UrlController');
 
 mongoose.connect(`${process.env.MONGO_HOST}/${process.env.MONGO_DB}`, {
   useNewUrlParser: true,
@@ -21,59 +19,9 @@ mongoose.connect(`${process.env.MONGO_HOST}/${process.env.MONGO_DB}`, {
 
 app.use(bodyParser());
 
-router.get('/', async (ctx) => {
-  ctx.body = {
-    data: 'URL Shorten API',
-  };
-});
-
-router.post('/shorten', async (ctx) => {
-  const { url } = ctx.request.body;
-
-  if (url && Url.isValid(url)) {
-    const expirationDate = getExpirationDate(process.env.SECONDS_TO_EXPIRE_URL);
-    const expirationTimeUnixTimestamp = getUnixTimestamp(expirationDate);
-
-    try {
-      const saved = await UrlModel.create({
-        url,
-        expires_at: expirationDate,
-      });
-
-      ctx.body = {
-        newUrl: `${process.env.SERVICE_URL}/${saved._id}`,
-        expiresAt: expirationTimeUnixTimestamp,
-      };
-    } catch (e) {
-      console.error(e);
-      ctx.throw(500, 'There was a problem with your request.');
-    }
-  } else {
-    ctx.status = 400;
-    ctx.body = {
-      message: 'Invalid request.'
-    };
-  }
-});
-
-router.get('/:id', async (ctx) => {
-  const { id } = ctx.params;
-
-  if (id) {
-    try {
-      const url = await UrlModel.findById(id);
-      if (url) {
-        ctx.redirect(url.url); // redirect to another page
-        return;
-      }
-    } catch (e) {
-      console.error(e);
-      ctx.throw(500, 'There was a problem with your request.');
-    }
-  }
-  ctx.body = { message: 'Url not found.' };
-  ctx.status = 404;
-});
+router.get('/health', urlController.health);
+router.post('/shorten', urlController.shorten);
+router.get('/:id', urlController.get);
 
 app.use(router.routes());
 
